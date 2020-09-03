f616b3979654cd7873e50cb148bfbc8b9c82142c
angela4321 <angelaqiao621@gmail.com>
2020-09-02 17:43:20 -0700

    Rough code for back prop for CNN

--- CNN.py
+++ CNN.py
@@ -10,11 +10,29 @@ class CNN(Layer):
         z = np.zeros((a.shape[0],a.shape[1]-2,a.shape[2]-2,a.shape[3]))
         for i in range(a.shape[0]): #iterate over num training
             for f in range(a.shape[3]):  # iterate over filters
-                for r in range(a.shape[1])-2:
-                    for c in range(a.shape[2])-2:
-                        step_a = a[i][r:r+w.shape[0]][c:c+w.shape[0]][f]
-                        z[i][r][c][f]=np.sum(np.multiply(step_a, w[f])) + b[f]
+                for r in range(z.shape[1]):
+                    for c in range(z.shape[2]):
+                        step_a = a[i][r:r+w.shape[0]][c:c+w.shape[0]]
+                        z[i][r][c][f]=np.sum(np.multiply(step_a, w[:][:][:][f])) + b[:][:][:][f]
         return z
+
+    def back_prop(self,dz,a,w):
+        da = np.zeros(a.shape)
+        dw = np.zeros(w.shape)
+        db = np.zeros((1,1,1,w.shape[3]))
+        for i in range(a.shape[0]):
+            for r in range(a.shape[1]):
+                for c in range(a.shape[2]):
+                    for f in range(dz.shape[3]):
+                        da[r:r+w.shape[0]][c:c+w.shape[0]]+= w[:][:][:][f] * dz[i][r][c][f]
+
+                        temp_a = a[i][r:r+w.shape[0]][c:c+w.shape[0]]
+                        dw[:][:][:][c]+=temp_a*dz[i][r][c][f]
+                        db[:][:][:][c]+=dz[i][r][c][f]
+
+        return da,dw,db
+
+
     def pool(self,a,size,stride):
         z = np.zeros((a.shape[0],math.floor((a.shape[1]-size)/stride)+1,math.floor((a.shape[2]-size)/stride)+1,a.shape[3]))
         for i in range(z.shape[0]): ##iterate over num training
